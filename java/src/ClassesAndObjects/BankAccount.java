28c1a002980cd1e0358cbf7eeb4751724bfc6f54
Advait Kamathe <akamathe@relatient.com>
2023-05-17 19:22:09 +0530

    Adding ClassesAndObjects

---
+++ java/src/ClassesAndObjects/BankAccount.java
@@ -0,0 +1,66 @@
+package ClassesAndObjects;
+
+import java.util.StringJoiner;
+
+public class BankAccount {
+  private static int accountNumber = 0;
+  private double accountBalance;
+  private final String customerName;
+  private String customerEmail;
+  private String phoneNumber;
+
+  public BankAccount(String customerName, String customerEmail, String phoneNumber) {
+    accountNumber++;
+    accountBalance = 0;
+    this.customerName = customerName;
+    this.customerEmail = customerEmail;
+    this.phoneNumber = phoneNumber;
+
+    System.out.println("New account created.");
+    System.out.println("Your account number is " + accountNumber);
+    System.out.println("You balance id $" + accountBalance);
+  }
+  public void depositFunds(double depositAmount) {
+    accountBalance += depositAmount;
+    System.out.println("Deposit of $" + depositAmount + " made.");
+    System.out.println("New balance is $" + accountBalance);
+  }
+
+  public void withdrawFunds(double withdrawAmount) {
+    if (withdrawAmount > accountBalance) {
+      System.out.println("Insufficient balance.");
+    } else {
+      accountBalance -= withdrawAmount;
+      System.out.println("Withdrawal of $" + withdrawAmount + " processed.");
+      System.out.println("New balance is $" + accountBalance);
+    }
+  }
+
+  public int getAccountNumber() {
+    return accountNumber;
+  }
+
+  public double getAccountBalance() {
+    return accountBalance;
+  }
+
+  public String getCustomerName() {
+    return customerName;
+  }
+
+  public String getCustomerEmail() {
+    return customerEmail;
+  }
+
+  public void setCustomerEmail(String customerEmail) {
+    this.customerEmail = customerEmail;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+}
---
+++ java/src/ClassesAndObjects/Main.java
@@ -0,0 +1,17 @@
+package ClassesAndObjects;
+
+public class Main {
+
+  public static void main(String[] args) {
+    BankAccount myBankAccount = new BankAccount("Advait",
+        "advait@gmail.com", "9876543210");
+
+    myBankAccount.depositFunds(1000);
+    myBankAccount.withdrawFunds(50);
+    myBankAccount.withdrawFunds(1000);
+
+    BankAccount anotherAccount = new BankAccount("Psi", "psi@gmail.com",
+        "9876543210");
+    anotherAccount.withdrawFunds(10);
+  }
+}
